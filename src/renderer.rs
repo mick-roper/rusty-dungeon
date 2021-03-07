99b912e2876993c3904deae4db620ab4fb32bfe4
mick-roper <mick@theropers.co.uk>
2021-03-07 20:35:35 +0000

    fixed the build

--- src/renderer.rs
+++ src/renderer.rs
@@ -9,15 +9,15 @@ use super::resource_management::*;
 
 type TextureManager<'l, T> = ResourceManager<'l, String, Texture<'l>, TextureCreator<T>>;
 
-pub struct Renderer<'l> {
+pub struct Renderer {
     context: sdl2::Sdl,
     canvas: sdl2::render::Canvas<sdl2::video::Window>,
     texture_creator: TextureCreator<WindowContext>,
-    texture_manager: TextureManager<'l, WindowContext>,
+    // texture_manager: TextureManager<'l, WindowContext>,
 }
 
-impl Renderer<'static> {
-    pub fn new(width: u32, height: u32) -> Result<Renderer<'static>, String> {
+impl Renderer {
+    pub fn new(width: u32, height: u32) -> Result<Renderer, String> {
         let context = sdl2::init()?;
         let video_system = context.video()?;
         let window = video_system
@@ -34,20 +34,19 @@ impl Renderer<'static> {
             .map_err(|e| e.to_string())?;
 
         let texture_creator = canvas.texture_creator();
-        let texture_manager = TextureManager::new(&texture_creator);
 
         canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
         canvas.clear();
         canvas.present();
 
-        Ok(Renderer{context, canvas, texture_creator, texture_manager})
+        Ok(Renderer{context, canvas, texture_creator})
     }
 
     pub fn event_pump(&mut self) -> Result<sdl2::EventPump, String> {
         self.context.event_pump()
     }
 
-    pub fn draw(&mut self, state: &mut State) {
+    pub fn draw(&mut self, state: &mut State) -> Result<(), String>{
         let positions = state.world.read_storage::<Position>();
         let drawables = state.world.read_storage::<Drawable>();
         let mut r1 = Rect::new(-1, -1, 0, 0);
@@ -60,12 +59,14 @@ impl Renderer<'static> {
             r2.set_y(pos.y);
             r2.set_width(drawable.width);
             r2.set_height(drawable.height);
-
-            let t = self.texture_manager.load(&drawable.texture_id).unwrap();
-
-            self.canvas.copy(&t, r1, r2);
+            let t = self.texture_creator.load("abc.png")?;
+            
+            self.canvas.copy(&t, r1, r2)?;
         }
 
         self.canvas.present();
+
+
+        Ok(())
     }
 }
