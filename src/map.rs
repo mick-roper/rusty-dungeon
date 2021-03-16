b1bc061383665a3f27cb2bd4704da99ef14ac0a4
mick-roper <mick@theropers.co.uk>
2021-03-16 20:48:34 +0000

    room generation has been added

--- src/map.rs
+++ src/map.rs
@@ -1,6 +1,5 @@
-use sdl2::rect::{Rect, Point};
-
-use super::texture_info::{VOID, TEXTURE_SIZE};
+use sdl2::rect::{Rect};
+use rand::{thread_rng, Rng};
 
 pub struct Map {
     pub width: u32,
@@ -12,28 +11,28 @@ pub struct Map {
 impl Map {
     pub fn new(width: u32, height: u32) -> Map {
         let mut tiles = vec![Tile{tile_type: TileType::Void}; (width * height) as usize];
-        let rooms = generate_rooms(1, width, height);
+        let rooms = generate_rooms(1, width / 10, height / 10);
 
-        // for room in rooms.iter() {
-        //     let r_x = room.x();
-        //     let r_x2 = r_x + room.width() as i32;
-        //     let r_y = room.y();
-        //     let r_y2 = r_y + room.height() as i32;
+        for room in rooms.iter() {
+            let r_x = room.x();
+            let r_x2 = r_x + room.width() as i32;
+            let r_y = room.y();
+            let r_y2 = r_y + room.height() as i32;
 
-        //     for x in r_x..r_x2 {
-        //         for y in r_y..r_y2 {
-        //             let idx = xy_idx(width, x, y);
-        //             let new_tile_type: TileType;
-        //             if x == r_x || x == r_x2 || y == r_y || y == r_y2 {
-        //                 new_tile_type = TileType::Wall;
-        //             } else {
-        //                 new_tile_type = TileType::Floor;
-        //             }
+            for x in r_x..r_x2 {
+                for y in r_y..r_y2 {
+                    let idx = xy_idx(width, x as u32, y as u32);
+                    let new_tile_type: TileType;
+                    if x == r_x || x == r_x2-1 || y == r_y || y == r_y2-1 {
+                        new_tile_type = TileType::Wall;
+                    } else {
+                        new_tile_type = TileType::Floor;
+                    }
 
-        //             tiles[idx].tile_type = new_tile_type;
-        //         }
-        //     }
-        // }
+                    tiles[idx].tile_type = new_tile_type;
+                }
+            }
+        }
 
         Map{width, height, tiles, rooms}
     }
@@ -62,9 +61,18 @@ pub enum TileType {
 
 fn generate_rooms(room_count: usize, width: u32, height: u32) -> Vec<Rect> {
     let mut rooms = Vec::new();
+    let mut rng = thread_rng();
+
+    let min_x = width / 10;
+    let min_y = height / 10;
 
     for _ in 0..room_count {
-        let r = Rect::new(200, 275, 15, 15);
+        let x: i32 = rng.gen_range(min_x as i32..(width as i32-min_x as i32));
+        let y: i32 = rng.gen_range(min_y as i32..(height as i32-min_y as i32));
+        let w: u32 = rng.gen_range(10..20);
+        let h: u32 = rng.gen_range(10..20);
+
+        let r = Rect::new(x, y, w, h);
         rooms.push(r);
     }
 
